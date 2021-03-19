88bb77ffdbb4932d8eac165d2f8456b968462aa8
Prashant <blackhole.gamma@gmail.com>
2021-03-19 21:42:22 +0530

    first commit

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
+
+function fibb(index) {
+  if (index < 2) return 1;
+  return fibb(index - 1) + fib(index - 2);
+}
+
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fibb(parseInt(message)));
+});
+
+sub.subscribe("insert");
