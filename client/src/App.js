56960a3949863d8aed5002565860e827ada62a05
Prashant <blackhole.gamma@gmail.com>
2021-03-27 13:28:53 +0530

    updated client

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,6 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
