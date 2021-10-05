5ea54730a5b80756da65ca1ad2ac03c84ac346fb
Prashant Kumar <prashantdrew23@gmail.com>
2021-10-05 13:00:44 +0530

    change heading

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Link to="/">Fibnnoci Calculator</Link>
+          <Link to="/">Fibnnoci Calculator</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
